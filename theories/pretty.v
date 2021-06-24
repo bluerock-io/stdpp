6de30407e4ab286a04423510d6d955a51ef9d9f2
Robbert Krebbers <mail@robbertkrebbers.nl>
2021-06-24 09:54:28 +0200

    Fix potential stack overflow related to `Pretty N`.

--- theories/pretty.v
+++ theories/pretty.v
@@ -23,7 +23,7 @@ Fixpoint pretty_N_go_help (x : N) (acc : Acc (<)%N x) (s : string) : string :=
   | right _ => s
   end.
 Definition pretty_N_go (x : N) : string → string :=
-  pretty_N_go_help x (wf_guard 32 N.lt_wf_0 x).
+  pretty_N_go_help x (wf_guard (S (N.size_nat x)) N.lt_wf_0 x).
 Global Instance pretty_N : Pretty N := λ x,
   if decide (x = 0)%N then "0" else pretty_N_go x "".
 

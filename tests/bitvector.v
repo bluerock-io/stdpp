f214fac6d6111618fbb47729e9c420e368c2e016
Michael Sammler <noreply@sammler.me>
2022-08-11 13:13:45 +0200

    Add bitvector library with tactics

---
+++ tests/bitvector.v
@@ -0,0 +1,11 @@
+From stdpp Require Import strings.
+From stdpp.unstable Require Import bitvector.
+
+Check "notation_test".
+Check (BV 10 3 = BV 10 5).
+Fail Check (BV 2 3 = BV 10 5).
+Check (BV 2 3 =@{bvn} BV 10 5).
+Fail Goal (BV 2 4 = BV 2 5).
+
+Check "bvn_to_bv_test".
+Goal bvn_to_bv 2 (BV 2 3) = Some (BV 2 3). Proof. simpl. Show. done. Abort.

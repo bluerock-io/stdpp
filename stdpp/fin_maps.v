b930e9a6528dc24c7dfdda3a08858f03b762585c
Dorian Lesbre <dorian.lesbre@gmail.com>
2023-03-20 12:38:28 +0100

    Simplify proof a bit using suggestions

--- stdpp/fin_maps.v
+++ stdpp/fin_maps.v
@@ -3819,13 +3819,13 @@ Section image.
   Lemma elem_of_img m v : v ∈ img m ↔ ∃ k, m !! k = Some v.
   Proof.
     unfold img, fin_map_img. rewrite elem_of_map_to_set. split.
-    - intros (k & v' & mk_v' & v'_v). exists k. rewrite v'_v in mk_v'. exact mk_v'.
-    - intros [k mk_v]. exists k. exists v. split; [exact mk_v|reflexivity].
+    - intros (k & v' & mk_v' & v'_v). exists k. by rewrite v'_v in mk_v'.
+    - intros [k mk_v]. exists k. by exists v.
   Qed.
   Lemma elem_of_img_1 m v : v ∈ img m → ∃ k, m !! k = Some v.
-  Proof. intros Hmkv. by rewrite <- elem_of_img. Qed.
+  Proof. by rewrite <- elem_of_img. Qed.
   Lemma elem_of_img_2 m k v : m !! k = Some v → v ∈ img m.
-  Proof. intros Hmkv. rewrite elem_of_img. exists k. exact Hmkv. Qed.
+  Proof. intros Hmkv. rewrite elem_of_img. by exists k. Qed.
 
   Lemma not_elem_of_img m v : v ∉ img m ↔ ∀ k, m !! k ≠ Some v.
   Proof.
@@ -3834,7 +3834,7 @@ Section image.
     - intros Hm [k Hk]. contradiction (Hm k Hk).
   Qed.
   Lemma not_elem_of_img_1 m v k : v ∉ img m → m !! k ≠ Some v.
-  Proof. intros Hm. apply not_elem_of_img. exact Hm. Qed.
+  Proof. by rewrite not_elem_of_img. Qed.
   Lemma not_elem_of_img_2 m v : (∀ k, m !! k ≠ Some v) → v ∉ img m.
   Proof. by rewrite <- not_elem_of_img. Qed.
 
@@ -3861,8 +3861,8 @@ Section image.
   Proof.
     rewrite set_equiv. intros x. split; intros Hempty.
     - apply elem_of_img in Hempty. destruct Hempty as [k Hk].
-      rewrite lookup_empty in Hk. discriminate.
-    - rewrite elem_of_empty in Hempty. contradiction Hempty.
+      by rewrite lookup_empty in Hk.
+    - by rewrite elem_of_empty in Hempty.
   Qed.
   Lemma img_empty_iff m : img m ≡ ∅ ↔ m = ∅.
   Proof.
@@ -3879,25 +3879,24 @@ Section image.
   Proof.
     intros w Hw. apply elem_of_img in Hw as [k' Hw].
     apply lookup_insert_Some in Hw as [[Hkk' Hvw] | [Hkk' Hmk']].
-    - apply elem_of_union_l, elem_of_singleton. symmetry. apply Hvw.
-    - apply elem_of_union_r, elem_of_img. exists k'. apply Hmk'.
+    - apply elem_of_union_l, elem_of_singleton. by symmetry.
+    - apply elem_of_union_r, elem_of_img. by exists k'.
   Qed.
   Lemma elem_of_img_insert k v m : v ∈ img (<[k:=v]> m).
   Proof. apply elem_of_img. exists k. apply lookup_insert. Qed.
   Lemma elem_of_img_insert_ne k v w m : v ≠ w → w ∈ img(<[k:=v]> m) → w ∈ img m.
   Proof.
-    rewrite !elem_of_img. intros Hv [k' Hk].
-    destruct (decide (k=k')) as [Heq|Heq].
-    - rewrite Heq, lookup_insert in Hk.
-      apply Some_inj in Hk. contradiction (Hv Hk).
-    - rewrite (lookup_insert_ne _ _ _ _ Heq) in Hk. exists k'. exact Hk.
+    intros Hv Hw. apply img_insert in Hw.
+    apply elem_of_union in Hw as [Hw|Hw].
+    - by apply elem_of_singleton in Hw.
+    - done.
   Qed.
   Lemma img_insert_notin k v m : m !! k = None → img (<[k:=v]> m) ≡ {[ v ]} ∪ img m.
   Proof.
     intros Hm w. rewrite elem_of_union, !elem_of_img, elem_of_singleton.
     setoid_rewrite lookup_insert_Some.
     split.
-    - intros [k' [[Hk Hv] | [Hk Hv]]]; [ (left; done) | right]. exists k'. done.
+    - intros [k' [[Hk Hv] | [Hk Hv]]]; [ (left; done) | right]. by exists k'.
     - intros [Hv | [k' Hv]]; [ (exists k; left; done) | (exists k'; right) ].
       destruct (decide (k=k')) as [Hk|Hk]; [ simplify_eq | done ].
   Qed.
@@ -3919,7 +3918,7 @@ Section image.
     destruct Hv12 as [k Hv12].
     destruct (lookup_union_Some_raw m1 m2 k v) as [Hspec _].
     destruct (Hspec Hv12) as [Hm | [_ Hm]];
-    [ left | right ]; rewrite elem_of_img; exists k; exact Hm.
+    [ left | right ]; rewrite elem_of_img; by exists k.
   Qed.
   Lemma elem_of_img_union_l m1 m2 v :
     v ∈ img m1 → v ∈ img (m1 ∪ m2).
@@ -3930,8 +3929,8 @@ Section image.
   Lemma elem_of_img_union_r m1 m2 v :
     m1 ##ₘ m2 → v ∈ img m2 → v ∈ img (m1 ∪ m2).
   Proof.
-    intros Hdisj Hv. rewrite elem_of_img. apply elem_of_img in Hv as [k Hv].
-    exists k. by apply lookup_union_Some_r.
+    intros Hdisj Hv. rewrite (map_union_comm _ _ Hdisj).
+    by apply elem_of_img_union_l.
   Qed.
   Lemma elem_of_img_union_disjoint m1 m2 v :
     m1 ##ₘ m2 → v ∈ img (m1 ∪ m2) ↔ v ∈ img m1 ∨ v ∈ img m2.
@@ -3973,27 +3972,27 @@ Section image.
       assert (IH' : img (list_to_map l) ≡ list_to_set l.*2).
       { apply IH. intros k' v' v'' Hv' Hv''. apply (Hl k' v' v''); apply elem_of_list_further; assumption. }
       rewrite <- IH'.
-      destruct ((list_to_map l : M A)!!k) as [w|] eqn:Hw.
+      destruct ((list_to_map l : M A) !! k) as [w|] eqn:Hw.
       + assert (Hvw : v = w).
         { apply (Hl k); [ apply elem_of_list_here | apply elem_of_list_further ].
-          apply elem_of_list_to_map_2. exact Hw. }
+          by apply elem_of_list_to_map_2. }
         rewrite <- Hvw in Hw. apply set_equiv. intros x. split.
-        * intros Hx. apply img_insert in Hx. done.
+        * intros Hx. by apply img_insert in Hx.
         * intros Hx. apply elem_of_union in Hx as [Hv | Hx].
           -- apply elem_of_singleton in Hv. rewrite Hv. apply elem_of_img_insert.
           -- apply elem_of_img in Hx as [k' Hx].
              destruct (decide (k=k'));  [(simplify_eq; apply elem_of_img_insert)|].
-              apply elem_of_img. exists k'. rewrite lookup_insert_ne; assumption.
-      + rewrite img_insert_notin; [reflexivity | exact Hw].
+              apply elem_of_img. exists k'. by rewrite lookup_insert_ne.
+      + by rewrite img_insert_notin.
   Qed.
 
   (** Alternative definition of [img] in terms of [map_to_list]. *)
   Lemma img_alt m : img m ≡ list_to_set (map_to_list m).*2.
   Proof.
     rewrite <-(list_to_map_to_list m) at 1.
-    rewrite img_list_to_map; [reflexivity|].
+    rewrite img_list_to_map; [done|].
     intros k v v' Hv Hv'. rewrite !elem_of_map_to_list in Hv, Hv'.
-    rewrite Hv in Hv'. apply Some_inj. exact Hv'.
+    rewrite Hv in Hv'. by apply Some_inj.
   Qed.
 
   Lemma img_singleton_inv m v :
@@ -4001,8 +4000,8 @@ Section image.
   Proof.
     intros Hm k. destruct (m!!k) eqn:Hmk.
     - right. apply elem_of_img_2 in Hmk. rewrite Hm, elem_of_singleton in Hmk.
-      rewrite Hmk. reflexivity.
-    - left. reflexivity.
+      by rewrite Hmk.
+    - by left.
   Qed.
 
   Lemma img_union_inv `{!RelDecision (∈@{D})} X Y m :
@@ -4019,7 +4018,7 @@ Section image.
     - apply Hdisj.
     - apply img_filter; intros v; split; [|naive_solver].
       intros Hv. apply (union_subseteq_l X Y) in Hv as Hv'. rewrite <-Himg, elem_of_img in Hv'.
-      destruct Hv' as [k Hk]. exists k. done.
+      destruct Hv' as [k Hk]. by exists k.
     - apply img_filter; intros v; split.
       + intros Hv.
         apply (union_subseteq_r X Y) in Hv as Hv'. rewrite <-Himg, elem_of_img in Hv'.

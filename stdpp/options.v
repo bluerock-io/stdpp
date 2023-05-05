283bda39319efb2d468e85dda9b977c277202f65
Ralf Jung <jung@mpi-sws.org>
2023-05-05 11:59:18 +0200

    add comments in options file

--- stdpp/options.v
+++ stdpp/options.v
@@ -3,10 +3,17 @@
 file is *imported*, the option will only apply on the import site
 but not transitively. *)
 
+(** Allow async proof-checking of sections. *)
 Export Set Default Proof Using "Type".
 (* FIXME: cannot enable this yet as some files disable 'Default Proof Using'.
 Export Set Suggest Proof Using. *)
+
+(** Enforces that every tactic is executed with a single focused goal, meaning
+that bullets and curly braces must be used to structure the proof. *)
 Export Set Default Goal Selector "!".
+
+(** Prevent using auto-generated names in proof scripts (or at least make it
+obvious when that happens) *)
 Export Set Mangle Names.
 Export Set Mangle Names Light.
 (** Make these names stand out more, in case one does end up in the proof script. *)

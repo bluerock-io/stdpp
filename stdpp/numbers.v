93177374e97c693a849b5da95600240bfa4a6183
Robbert Krebbers <gitlab-sws@robbertkrebbers.nl>
2023-08-30 12:10:23 +0000

    improve comment

--- stdpp/numbers.v
+++ stdpp/numbers.v
@@ -47,13 +47,20 @@ Reserved Notation "x ≤ y ≤ z ≤ z'"
 
 Infix "≤" := le : nat_scope.
 (** We do *not* add notation for [≥] mapping to [ge], and we do also not use the
-[>] notation. Using such notation leads to annoying problems: if you have [x <
-y] in the context and need [y > x] for some lemma, [assumption] won't work, and
-it's just generally very frustrating to deal with this mismatch. It is
-preferable to state logically equivalent things in syntactically equal ways. We
-could define [>] and [≥] as [parsing only notation] that maps to [<] and [≤],
-respectively, but that would mean changing the Coq notation for [<] to something
-that is not even definitionally equal, so we avoid that as well. *)
+[>] notation from the Coq standard library. Using such notations leads to
+annoying problems: if you have [x < y] in the context and need [y > x] for some
+lemma, [assumption] won't work because [x < y] and [y > x] are not
+definitionally equal. It is just generally frustrating to deal with this
+mismatch, and much preferable to state logically equivalent things in syntactically
+equal ways.
+
+As an alternative, we could define [>] and [≥] as [parsing only] notation that
+maps to [<] and [≤], respectively (similar to math-comp). This would change the
+notation for [<] from the Coq standard library to something that is not
+definitionally equal, so we avoid that as well.
+
+This concern applies to all number types: [nat], [N], [Z], [positive], [Qc] and
+[Qp]. *)
 Notation "x ≤ y ≤ z" := (x ≤ y ∧ y ≤ z)%nat : nat_scope.
 Notation "x ≤ y < z" := (x ≤ y ∧ y < z)%nat : nat_scope.
 Notation "x < y ≤ z" := (x < y ∧ y ≤ z)%nat : nat_scope.

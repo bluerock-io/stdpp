415ba1b5d9eae3441946ec0ddc29d98ceb14a368
Robbert Krebbers <mail@robbertkrebbers.nl>
2023-09-11 14:29:26 +0200

    Add instance `bv_unsigned_inj` and use that.

--- stdpp_unstable/bitvector.v
+++ stdpp_unstable/bitvector.v
@@ -312,6 +312,9 @@ Lemma bv_neq n (b1 b2 : bv n) :
   b1 ≠ b2 ↔ b1.(bv_unsigned) ≠ b2.(bv_unsigned).
 Proof. unfold not. by rewrite bv_eq. Qed.
 
+Global Instance bv_unsigned_inj n : Inj (=) (=) (@bv_unsigned n).
+Proof. intros ???. by apply bv_eq. Qed.
+
 Definition Z_to_bv_checked (n : N) (z : Z) : option (bv n) :=
   guard (BvWf n z) as H; Some (@BV n z H).
 
@@ -1149,7 +1152,7 @@ Section little.
     m = Z.of_nat (length bs) →
     bv_to_little_endian m n (little_endian_to_bv n bs) = bs.
   Proof.
-    intros ->. eapply (fmap_eq_inj bv_unsigned). { intros ???. by apply bv_eq. }
+    intros ->. apply (inj (fmap bv_unsigned)).
     rewrite bv_to_litte_endian_unsigned; [|lia].
     apply Z_to_little_endian_to_Z; [by rewrite fmap_length | lia |].
     apply Forall_forall. intros ? [?[->?]]%elem_of_list_fmap_2. apply bv_unsigned_in_range.

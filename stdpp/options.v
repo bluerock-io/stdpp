60bf06688cf397092ad762c5f6d40628ced79c0e
Ralf Jung <jung@mpi-sws.org>
2023-09-14 14:32:30 +0200

    deny more warnings

--- stdpp/options.v
+++ stdpp/options.v
@@ -10,13 +10,13 @@ file is *imported*, the option will only apply on the import site
 but not transitively. *)
 
 (** Allow async proof-checking of sections. *)
-Export Set Default Proof Using "Type".
+#[export] Set Default Proof Using "Type".
 (* FIXME: cannot enable this yet as some files disable 'Default Proof Using'.
-Export Set Suggest Proof Using. *)
+#[export] Set Suggest Proof Using. *)
 
 (** Enforces that every tactic is executed with a single focused goal, meaning
 that bullets and curly braces must be used to structure the proof. *)
-Export Set Default Goal Selector "!".
+#[export] Set Default Goal Selector "!".
 
 (* "Fake" import to whitelist this file for the check that ensures we import
 this file everywhere.
